d3d31dcbc39ddd9611229b7bd92c599ac1a235e1
seb <seb.bacon@gmail.com>
2016-04-14 09:44:10 +0100

    Further validators used in openc schemas

--- lib/openc_json_schema_formats.rb
+++ lib/openc_json_schema_formats.rb
@@ -1,5 +1,6 @@
 require "openc_json_schema_formats/version"
 require "json_validation"
+require "uri"
 
 module OpencJsonSchemaFormats
   class DateFormatValidator
@@ -10,6 +11,32 @@ module OpencJsonSchemaFormats
       false
     end
   end
+
+  class NonBlankValidator
+    def validate(record)
+      !(record.is_a?(String) && record.strip == '')
+    end
+  end
+
+  class URIValidator
+    # Actually, this only checks http and https formats, so isn't
+    # really for URIs
+    def validate(record)
+      begin
+        uri = URI.parse(record)
+        uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+      rescue URI::InvalidURIError
+        false
+      end
+    end
+  end
 end
 
-JsonValidation.add_format_validator('date', OpencJsonSchemaFormats::DateFormatValidator)
+JsonValidation.add_format_validator(
+  'date', OpencJsonSchemaFormats::DateFormatValidator)
+
+JsonValidation.add_format_validator(
+  'non-blank', OpencJsonSchemaFormats::NonBlankValidator)
+
+JsonValidation.add_format_validator(
+  'uri', OpencJsonSchemaFormats::URIValidator)
--- spec/openc_json_schema_formats_spec.rb
+++ spec/openc_json_schema_formats_spec.rb
@@ -26,4 +26,45 @@ describe OpencJsonSchemaFormats do
       end
     end
   end
+
+  describe 'nonblank validator' do
+    before do
+      @validator = JsonValidation.build_validator({'format' => 'non-blank'}, strict_format_validation: true)
+    end
+
+    context 'valid objects' do
+      [123, nil, "tofu"].each do |object|
+        it "returns true for `#{object}`" do
+          expect(@validator.validate(object)).to eq(true)
+        end
+      end
+    end
+
+    it "returns false for empty string" do
+      expect(@validator.validate("")).to eq(false)
+    end
+  end
+
+  describe 'URI validator' do
+    before do
+      @validator = JsonValidation.build_validator({'format' => 'uri'}, strict_format_validation: true)
+    end
+
+    context 'valid uris' do
+      ["http://foo.bar/?baz=qux#quux", "https://foo.bar/?baz=qux#quux"].each do |uri|
+        it "returns true for `#{uri}`" do
+          expect(@validator.validate(uri)).to eq(true)
+        end
+      end
+    end
+
+    context 'invalid uris' do
+      ["\\\\WINDOWS\\fileshare", "tofu", "http://foo.b::ar/?baz=qux#quux"].each do |uri|
+        it "returns false for `#{uri}`" do
+          expect(@validator.validate(uri)).to eq(false)
+        end
+      end
+    end
+  end
+
 end
